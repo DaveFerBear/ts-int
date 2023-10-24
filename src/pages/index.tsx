ebe84e01adfa1f813ab19388740f051794e16413
David Ferris <david@phonic.ai>
2023-10-24 12:55:27 -0400

    add p3 (WIP)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,8 +9,8 @@ export default function Home() {
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          This is a&nbsp;
-          <code className="font-mono font-bold">demo app</code>
+          Playground&nbsp;
+          <code className="font-mono font-bold">virtual on-site</code>
         </p>
       </div>
     </main>
---
+++ src/utils/p3.tsx
@@ -0,0 +1,11 @@
+/**
+ * Follow-up items:
+ *
+ *  - What if /dowork takes a long time to respond (30+ seconds)? How should we handle this?
+ *  - What if /dowork is not deterministic or sometimes errors?
+ *  - Let's add some features:
+ *   - cache expiry/invalidation
+ *   - cache serialization (files, redis, LS, etc.)
+ *   - analytics/logging
+ *
+ */
